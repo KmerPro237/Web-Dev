3054f69d85ef79a594a4d2b0b5b05d4bcafecf46
KmerPro237 <fotsososthene02@gmail.com>
2023-11-30 16:04:27 +0100

    Inner HTML

--- Web-Dev/JavaScript/index.js
+++ Web-Dev/JavaScript/index.js
@@ -13,6 +13,6 @@ console.log("Hello", firstName);
 console.log("You are", age, "years old");
 console.log("Enrolled:", student);
 
-//document.getElementById("p1").innerHTML = "Hello " + firstName;
-//document.getElementById("p2").innerHTML = "You are " + age + " years old";
-//document.getElementById("p3").innerHTML = "Enrolled: " + student;
+document.getElementById("p1").innerHTML = "Hello2 " + firstName;
+document.getElementById("p2").innerHTML = "You are " + age + " years old";
+document.getElementById("p3").innerHTML = "Enrolled: " + student;
