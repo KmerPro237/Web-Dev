11114f20d85faa114e0c6ebd1a2d6a2dd0218918
KmerPro237 <fotsososthene02@gmail.com>
2023-12-01 10:34:34 +0100

    Constants

---
+++ Web-Dev/JavaScript/MathFunctions.js
@@ -0,0 +1,22 @@
+let x = 3.65;
+let p = 2.2;
+
+x = Math.round(x);
+let y = Math.floor(2);
+let z = Math.ceil(x);
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+x = Math.pow(2, 3);
+console.log(x);
+x = Math.sqrt(9);
+console.log(x);
+
+s = Math.random();
+console.log(s);
+
+s = s**2;
+console.log(s);
+console.log(Math.abs(-5));
