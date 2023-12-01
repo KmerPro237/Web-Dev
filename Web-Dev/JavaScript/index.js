7cc1b2c631e58493d066b26cf97299621dc91106
KmerPro237 <fotsososthene02@gmail.com>
2023-12-01 09:18:19 +0100

    let vs var

--- Web-Dev/JavaScript/index.js
+++ Web-Dev/JavaScript/index.js
@@ -13,6 +13,27 @@ console.log("Hello", firstName);
 console.log("You are", age, "years old");
 console.log("Enrolled:", student);
 
-document.getElementById("p1").innerHTML = "Hello2 " + firstName;
-document.getElementById("p2").innerHTML = "You are " + age + " years old";
-document.getElementById("p3").innerHTML = "Enrolled: " + student;
+//document.getElementById("p1").innerHTML = "Hello2 " + firstName;
+//document.getElementById("p2").innerHTML = "You are " + age + " years old";
+//document.getElementById("p3").innerHTML = "Enrolled: " + student;
+
+// let vs var
+function testVar() {
+    if (true) {
+      var x = 5;
+      console.log("Value of var within block: ----> "+ x); // 5
+    }
+    console.log("Value of var outside block but same scope: ----> "+x); // 5 (var is function-scoped)
+  }
+
+  function testLet() {
+    if (true) {
+      let y = 10;
+      console.log("Value of let within block: ----> " +y); // 10
+    }
+    console.log("Value of var within block: ----> " +y); // ReferenceError: y is not defined (let is block-scoped)
+  }
+
+  console.log(testLet());
+  console.log(testVar());
+  
